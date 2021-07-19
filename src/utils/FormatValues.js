3bf1c95caa0a1d45b0427341b7618d8764233629
Omar Kraidie <omppula_1@hotmail.com>
2021-07-19 17:22:05 +0300

    Added error validation

--- src/utils/FormatValues.js
+++ src/utils/FormatValues.js
@@ -1,21 +1,36 @@
 export const formatPrice = (value, locale, currency) => {
 	let price;
-	if (value < 1000) {
-		price = new Intl.NumberFormat(locale, {
-			style: 'currency',
-			currency: currency,
-			maximumFractionDigits: 4,
-		}).format(value);
+	if (value) {
+		if (value < 0.0001) {
+			price = new Intl.NumberFormat(locale, {
+				style: 'currency',
+				currency: currency,
+				maximumFractionDigits: 6,
+			}).format(value);
+		} else if (value < 1000) {
+			price = new Intl.NumberFormat(locale, {
+				style: 'currency',
+				currency: currency,
+				maximumFractionDigits: 4,
+			}).format(value);
+		} else {
+			price = new Intl.NumberFormat(locale, {
+				style: 'currency',
+				currency: currency,
+			})
+				.format(value)
+				.replace(/(\.|,)\d+$/g, '');
+		}
+		return price;
 	} else {
-		price = new Intl.NumberFormat(locale, {
-			style: 'currency',
-			currency: currency,
-		})
-			.format(value)
-			.replace(/(\.|,)\d+$/g, '');
+		return 'No data...';
 	}
-
-	return price;
 };
 
-export const formatPercent = (number) => `${number.toFixed(2)}%`;
+export const formatPercent = (number) => {
+	if (number) {
+		return 'No data...' && `${number.toFixed(2)}%`;
+	} else {
+		return 'No data...';
+	}
+};
