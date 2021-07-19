decd1ccdbee3f3fac89f450d66d6695bfff6ae58
Omar Kraidie <omppula_1@hotmail.com>
2021-07-19 15:46:38 +0300

    Added logo & named APP

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faDollarSign, faSortDown } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../images/logo.png';
 import './Nav.css';
 
 const Nav = () => {
 	return (
 		<nav className="navbar">
-			<div className="info">
-				<a href="">
-					<img
-						src="https://www.brahenvoimistelijat.fi/wp_public/wp-content/uploads/2018/01/logo.png"
-						alt="logo.jpg"
-						width="40px"
-					/>
-					<h1>site name</h1>
+			<div className="nav-info">
+				<a href=".">
+					<img src={logo} alt="logo.jpg" />
+					<h1>
+						<span>C</span>oin <span>C</span>aps
+					</h1>
 				</a>
 			</div>
 			<div className="controls">
