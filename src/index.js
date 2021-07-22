8cb3cafda12110e17ef696a9cf22f1e9b171d9a6
Omar Kraidie <omppula_1@hotmail.com>
2021-07-22 08:58:32 +0300

    Edited directory paths

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './components/loaders/Loaders.css';
 import './components/table/Table.css';
-import './components/controls/SearchBar.css';
+import './components/search/SearchBar.css';
 import './components/nav/Nav.css';
 
 import App from './App';
