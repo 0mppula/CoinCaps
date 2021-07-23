fb5146ac8f5c355f9d65d70b0810b71293bace8c
Omar Kraidie <omppula_1@hotmail.com>
2021-07-23 17:00:09 +0300

    Created ToTop

--- src/components/tools/ToTop.js
+++ src/components/tools/ToTop.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 const ToTop = () => {
+	const [active, setActive] = useState(false);
+
+	window.addEventListener('scroll', () => {
+		let pageY = window.pageYOffset;
+		pageY > 450 && setActive(true);
+		pageY < 450 && setActive(false);
+	});
+
+	const scrollToTop = () => {
+		window.scrollTo(0, 0);
+	};
+
 	return (
-		<div className="to-top-button">
+		<div
+			tabIndex={active ? 0 : -1}
+			onClick={scrollToTop}
+			onKeyPress={scrollToTop}
+			className={`to-top-button ${active ? 'active' : ''}`}
+		>
 			<FontAwesomeIcon icon={faChevronUp} />
 		</div>
 	);
